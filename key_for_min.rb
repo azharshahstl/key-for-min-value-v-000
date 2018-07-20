c713b0de705ec3ae42339ef0dbf2edb8a46bf595
Azhar Shah <azharshahstl@yahoo.com>
2018-07-20 19:11:25 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,8 @@
 def key_for_min_value(name_hash)
   if name_hash == {}
       return nil
-  name_hash.each do |name, number|
+  else
+    name_hash.each do |name, number|
     number 
     x = 0 
     while x < number.length-2
