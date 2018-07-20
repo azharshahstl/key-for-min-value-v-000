7d5cb5d243b53b41d61b1dc5f5964cb6309ae61f
Azhar Shah <azharshahstl@yahoo.com>
2018-07-20 18:54:53 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,5 +4,13 @@
 def key_for_min_value(name_hash)
   name_hash.each do |name, number|
     number 
-    
+    x = 0 
+    while x < number.length-2
+        if number[x] < number[x + 1]
+            swap = number[x]
+            number[x] = number[x+1]
+            number[x + 1] = swap
+        end
+    end
+end
 end
