807fb9439c219ae404ed47db4f4e08f3c09d6225
Azhar Shah <azharshahstl@yahoo.com>
2018-07-20 20:32:34 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,6 +8,7 @@ def key_for_min_value(name_hash)
     numbers = []
     name_hash.each do |name, number|
     numbers << number
+    names << name
     end
       numbers
       x = 0 
@@ -15,8 +16,8 @@ def key_for_min_value(name_hash)
           if numbers[0] < numbers[x + 1]
              numbers
           else
-             numbers[x + 1] = numbers[0] 
-             numbers[0] = numbers[x + 1] 
+             numbers[x + 1] = numbers[0] && names[x + 1] = names[0] 
+             numbers[0] = numbers[x + 1] && names[0] = names[x +1]
           end
       x += 1 
       end
